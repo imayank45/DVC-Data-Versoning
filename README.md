ca52459f75733edfb4710b671cc95d68b0c3554e
Mayank <mkathane84@mail.com>
2025-01-19 14:33:57 +0530

    first commit before DVC

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DVC-Data-Versoning
+
+## Step1:
+Create git repository and clone it to the local
+
+## Step2:
+Create a python file (copy.py) and add code to it (i.e it will save a csv file to a new data folder).
+
+## Step3:
+Do the git add and commit before initializing the DVC
+
---
+++ my_code.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+# create a sample dataset
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+
+# Adding new row to the dataset v2
+# new_row_loc1 = {'Name': ['Alice'], 'Age':20, 'City': 'Mumbai'}
+# df.loc[len(df.index)] = new_row_loc1
+
+## Adding new row to the dataset v3
+# new_row_loc2 = {'Name': 'Mahesh', 'Age':25, 'City': 'Dubai'}
+# df.loc[len(df.index)] = new_row_loc2
+
+# Ensure the data directory exists at the root level
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+# define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# save the dataframe to a CSV file, including the columns name
+df.to_csv(file_path, index=False)
+
+print(f"CSV file saved to {file_path}")
